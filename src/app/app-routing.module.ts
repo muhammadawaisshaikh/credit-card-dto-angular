fffbb78342691f3155c2b662cf2a00f65b64bacf
awaisshaikh.official@gmail.com <ilyasahmed081@gmail.com>
2021-02-07 21:07:23 +0500

    lazy-loaded modules added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./homepage/homepage.module').then(m => m.HomepageModule)
+  },
+  {
+    path: 'homepage',
+    loadChildren: () => import('./homepage/homepage.module').then(m => m.HomepageModule)
+  },
+  {
+    path: 'payment',
+    loadChildren: () => import('./payment/payment.module').then(m => m.PaymentModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
